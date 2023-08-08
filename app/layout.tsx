c76494b8373b79d5e26dae225256de8bf387b5ef
Conor <conor@visibleone.com>
2023-08-08 15:51:30 +0800

    SEO: Implement SEO and GA4 Scripts :white_check_mark:

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,62 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "./lib/auth";
 import Image from "next/image";
 import { Logout, NavLogin } from "./components/Button";
+import Link from "next/link";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CL Chat",
-  description: "Modern chat app built with Next.js and Tailwind CSS.",
+  title: "CL Chat - Simple chat app built with Next.js and Tailwind CSS",
+  description:
+    "Modern chat app built with Next.js and Tailwind CSS and powered by NextAuth.js and Supabase with Pusher for realtime updates.",
+  keywords: [
+    "nextjs",
+    "tailwindcss",
+    "nextauth",
+    "supabase",
+    "pusher",
+    "chat",
+    "realtime",
+    "react",
+    "typescript",
+    "nextjs starter",
+    "nextjs template",
+    "tailwindcss starter",
+    "tailwindcss template",
+    "nextjs tailwindcss starter",
+    "nextjs tailwindcss template",
+    "nextjs tailwindcss starter template",
+    "nextjs tailwindcss template starter",
+    "nextjs tailwindcss starter template with nextauth",
+    "nextjs tailwindcss template with nextauth",
+    "nextjs tailwindcss starter template with nextauth",
+    "nextjs tailwindcss template starter with nextauth",
+    "nextjs tailwindcss starter template with nextauth and supabase",
+    "nextjs tailwindcss template with nextauth and supabase",
+    "nextjs tailwindcss starter template with nextauth and supabase",
+    "nextjs tailwindcss template starter with nextauth and supabase",
+    "nextjs tailwindcss starter template with nextauth supabase and pusher",
+    "nextjs tailwindcss template with nextauth supabase and pusher",
+    "nextjs tailwindcss starter template with nextauth supabase and pusher",
+    "nextjs tailwindcss template starter with nextauth supabase and pusher",
+  ],
+  publisher: "CL Chat",
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: "https://cl-chat.vercel.app/",
+    title: "CL Chat - Simple chat app built with Next.js and Tailwind CSS",
+    description: `Modern chat app built with Next.js and Tailwind CSS and powered by NextAuth.js and Supabase with Pusher for realtime updates.`,
+    images: [
+      {
+        url: "https://i.imgur.com/9sdQpCG.png",
+        width: 1200,
+        height: 630,
+        alt: "CL Chat - Simple chat app built with Next.js and Tailwind CSS",
+      },
+    ],
+  },
 };
 
 export default async function RootLayout({
@@ -25,11 +75,26 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <NextAuthProvider>
+          {/* Add Google Analytics script */}
+          <Script
+            strategy="afterInteractive"
+            src={`https://www.googletagmanager.com/gtag/js?id=G-W67JJJTSNL`}
+          />
+          <Script strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-W67JJJTSNL');
+            `}
+          </Script>
           {/* Navbar */}
           <nav className="flex px-10 py-5 justify-between fixed top-0 left-0 w-full bg-white">
-            <h1 className="text-black text-3xl font-bold">
-              CL<span className="text-blue-500">Chat</span>
-            </h1>
+            <Link href="/">
+              <h1 className="text-black text-3xl font-bold">
+                CL<span className="text-blue-500">Chat</span>
+              </h1>
+            </Link>
 
             {session ? (
               <div className="flex items-center">
